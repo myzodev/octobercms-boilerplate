ba3fe869d0848d1dac9a1f2daf146037c730357a
Michal Valo <michal.valo6@gmail.com>
2024-09-08 12:44:27 +0200

    Change scripts placeholder

--- plugins/webpage/cookies/components/CookiesBar.php
+++ plugins/webpage/cookies/components/CookiesBar.php
@@ -18,9 +18,7 @@ class CookiesBar extends BaseComponent
 
     public function onRun(): void
     {
-        // dd($this->cookiesConsent);
-        $path = "/plugins/webpage/cookies/assets/js/cookie-bar.js";
-        $this->addJs($path, ['defer' => true]);
+        $this->addJs('/plugins/webpage/cookies/assets/js/cookie-bar.js');
     }
 
     public function componentDetails(): array
@@ -39,26 +37,26 @@ class CookiesBar extends BaseComponent
         array_pop($cookies); // Remove consent (array_pop since consent is always last)
 
         // Allow all cookies
-        if($consent === 'allow-all') {
+        if ($consent === 'allow-all') {
             $cookies = CookieSetting::getCookies();
         }
 
         // Allow only required cookies
-        if($consent === 'deny-all') {
+        if ($consent === 'deny-all') {
             $cookies = CookieSetting::getRequiredCookies();
         }
 
         // Allow selection and check if any cookies have been accepted
-        if($consent === 'allow-selection') {
+        if ($consent === 'allow-selection') {
             $cookies = CookieSetting::getSelectedCookies($cookies);
         }
 
-        if(empty($cookies)) {
+        if (empty($cookies)) {
             $cookies = CookieSetting::getRequiredCookies();
         }
 
         CookieSetting::setCookies($cookies);
-        
+
         $this->setVariable('acceptedCookies', CookieSetting::getSelectedCookies($cookies));
     }
 }
