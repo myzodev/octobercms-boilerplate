b98da345e21469327b52d7eda3a0e53a9ccb6422
Michal Valo <michal.valo6@gmail.com>
2026-01-19 14:20:53 +0100

    chore: update dependencies & fixes

--- themes/theme/vite.config.ts
+++ themes/theme/vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
-import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
 
 const input = {
 	js: resolve(__dirname, 'src/js/app.ts'),
@@ -9,21 +9,19 @@ const input = {
 
 const themeName = 'theme'
 
-export default {
+export default defineConfig({
 	base: `/themes/${themeName}/assets/build/`,
 	build: {
 		rollupOptions: { input },
 		manifest: true,
 		emptyOutDir: true,
 		outDir: resolve(__dirname, 'assets/build'),
-        assetsDir: '',
+		assetsDir: '',
 	},
 	server: {
 		hmr: {
 			protocol: 'ws',
 		},
 	},
-    plugins: [
-        tailwindcss(),
-    ],
-} satisfies UserConfig
+	plugins: [tailwindcss()],
+})
